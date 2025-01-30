4259d366cab2caa41b7cea6b87e06a5dcac1e9de
aiden.sim(심준보)/kakao <aiden.sim@kakaocorp.com>
2025-01-30 14:59:19 +0900

    project initializer

---
+++ src/main/java/com/aiden/domain/Campaign.java
@@ -0,0 +1,12 @@
+package com.aiden.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@ToString
+public class Campaign {
+    Long id;
+}
